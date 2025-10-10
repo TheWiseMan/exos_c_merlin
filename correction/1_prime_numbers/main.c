daf2e18c10b91557697b5775666accfc6d985a14
jdg <jdgrillet22@gmail.com>
2025-10-10 10:04:03 +0200

    exemple correction

---
+++ correction/1_prime_numbers/main.c
@@ -0,0 +1,15 @@
+int main() {
+    int N = 100;
+    int liste[] = {0};
+
+    int X = 2;
+    for (int i = 0; i<N; i++) {
+        for (/* tous elts k dans liste*/) {
+            // X est divisible par liste[k] ?
+            // si oui, X est pas premier
+        }
+        // sinon X est premier
+        // on ajoute X à la liste
+        X++;
+    }
+}
