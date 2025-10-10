65be2acdc78512f5de1a80fd36c34de32d423947
jdg <jdgrillet22@gmail.com>
2025-10-10 10:18:38 +0200

    modification correction partielle

--- correction/1_prime_numbers/main.c
+++ correction/1_prime_numbers/main.c
@@ -2,14 +2,16 @@ int main() {
     int N = 100;
     int liste[] = {0};
 
+    int i = 0;
     int X = 2;
-    for (int i = 0; i<N; i++) {
+    while (i<N) {
         for (/* tous elts k dans liste*/) {
             // X est divisible par liste[k] ?
             // si oui, X est pas premier
         }
         // sinon X est premier
         // on ajoute X à la liste
+        // i++
         X++;
     }
 }
